979fc18b8784c387a9a3613a63bf9e07d2ca770a
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-29 13:57:22 -0700

    Added some constants to be used

--- main.cpp
+++ main.cpp
@@ -7,6 +7,28 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <queue>
+
+// Size of the FIFO paging queue to be simulated
+const uint16_t QUEUE_SIZE = 1000;
+
+// Minimum page id
+const uint16_t MIN_PAGE_NUM = 1;
+
+// Maximum page id
+const uint16_t MAX_PAGE_NUM = 250;
+
+// Minimum number of memory frames to be simulated
+const uint16_t MIN_FRAMES = 1;
+
+// Maximum number of memory frames to be simulated
+const uint16_t MAX_FRAMES = 100;
+
+// The number of frames to step by during the simulation
+const uint16_t STEP_BY = 1;
+
+// The number of page sequences to be generated and simulated
+const uint16_t NUM_SEQUENCES = 100;
 
 int main() {
     return EXIT_SUCCESS;
