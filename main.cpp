456984218d19ed8a136cad2e76219687f4281ede
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-11-29 13:36:39 -0700

    Began Assign6

---
+++ main.cpp
@@ -0,0 +1,14 @@
+///////////////////////////
+// Tanner Kvarfordt      //
+// A02052217             //
+// CS 3100 --Dr. Mathias //
+// Assignment 4          //
+///////////////////////////
+
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    return EXIT_SUCCESS;
+}
+
